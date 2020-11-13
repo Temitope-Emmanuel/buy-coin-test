fadf5affc66a9248f002e6b54ed60310483ca11c
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-11-13 18:29:51 +0100

    removed token

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// require('isomorphic-unfetch')
-const PERSONAL_TOKEN = "c9ee36974b3bf4c57b08eb00dd3a1fda451a59d8"
 
 
 // Select the image tags
 const avatarDisplay = document.querySelectorAll(".profile-Image")
 // Select containing bio for profile detail
 const ownerDetail = document.querySelector(".repo-owner")
+// To display thr extra owner detail
+const followingAmount = document.querySelector("#following-amount")
+const followerAmount = document.querySelector("#follower-amount")
+const starredAmount = document.querySelector("#starredAmount")
+const locationDetail = document.querySelector("#location-detail")
+
 // Select display for the amount of repo
 const repoAmount = document.querySelectorAll(".repo-amount")
 
+
+
 // Get Tag to display mini detail 
 const miniDetail = document.querySelector(".mini-detail")
-// Select tag to display the repo detail
 
+
+// Select tag to display the repo detail
 const repoNames = document.querySelectorAll(".repo-name")
 const primaryLanguages = document.querySelectorAll(".primary-language")
 const languageColor = document.querySelectorAll(".language-color")
@@ -26,7 +33,7 @@ const getApiRequest = () => {
       fetch(url,{
           method:"POST",
           headers:{
-              'Authorization':`Bearer 64f798f88d9b55df41fee1fee72397183b981f7a`
+              'Authorization':`Bearer <<Check Comments For Token>>`
           },
           body:JSON.stringify({
               query:`{
@@ -35,6 +42,16 @@ const getApiRequest = () => {
                   login
                   avatarUrl
                   bio
+                  followers{
+                    totalCount
+                  }
+                  following{
+                    totalCount
+                  }
+                  location
+                  starredRepositories{
+                    totalCount
+                  }
                   repositories(first:20){
                     edges{
                       node{
@@ -61,7 +78,13 @@ const getApiRequest = () => {
       ownerDetail.children[1].textContent = payload.data.viewer.login
       miniDetail.children[1].textContent = payload.data.viewer.login
       ownerDetail.children[2].textContent = payload.data.viewer.bio
-      
+      followingAmount.textContent = payload.data.viewer.following.totalCount
+      followerAmount.textContent = payload.data.viewer.followers.totalCount
+      starredAmount.textContent = payload.data.viewer.starredRepositories.totalCount
+      locationDetail.textContent = payload.data.viewer.location
+
+
+
       // For setting the image src attr
       for(let i = 0; i < avatarDisplay.length ; i++){
         avatarDisplay[i].setAttribute("src",payload.data.viewer.avatarUrl)
